0195beb32531ce86de64702eb779878a70565acb
simplexduari <97849574+simplexduari@users.noreply.github.com>
2023-06-27 09:35:16 +0530

    Update 1_to_N_print_.cpp

--- 1_to_N_print_.cpp
+++ 1_to_N_print_.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    int n;
+    cout << "Enter a positive integer : ";
+    cin >> n;
+    int i = 1;
+loop:
+    if (i <= n)
+    {
+        cout << i << ' ';
+        i++;
+        goto loop;
+    }
+    cout << '\n';
+    return 0;
+}
